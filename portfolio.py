e29ebcf685caad9203b9771eb79a98c07b920b72
AravindhP2007 <aravindhpoobalan2007@gmail.com>
2024-11-25 19:27:44 +0530

    Initial Commit

---
+++ portfolio.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title='Portfolio')
+
+st.header('Aravindh\'s Portfolio')
+
+st.subheader('My Bio')
+
+st.markdown('Name : Aravindh.P')
+st.markdown('Studies : 1st Year AI&DS')
+st.markdown('College : K.S.Rangasamy College of Technology ,KSR Kalvi Nagar,Tirchungode - 638215')
+st.subheader('Skills')
+st.subheader('Projects')
