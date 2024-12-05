66a1c5e1b65b5a0020e5488869dabffa24591e33
AravindhP2007 <aravindhpoobalan2007@gmail.com>
2024-12-05 22:02:34 +0530

    portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -9,5 +9,15 @@ st.subheader('My Bio')
 st.markdown('Name : Aravindh.P')
 st.markdown('Studies : 1st Year AI&DS')
 st.markdown('College : K.S.Rangasamy College of Technology ,KSR Kalvi Nagar,Tirchungode - 638215')
+
 st.subheader('Skills')
 st.subheader('Projects')
+col1,col2=st.columns([1,10])
+with col1:
+    pass
+with col2:
+    st.markdown('AI Chatbot')
+    st.text('Still in a project . Not yet  completed')
+
+if st.checkbox('Rate my portfolio'):
+    st.radio('choose',('Perfect','Excellent','Good','Not Satisfied','Bad'))
