1a3a2a7ff4718929b5412518f0c663a2f091be35
AravindhP2007 <aravindhpoobalan2007@gmail.com>
2024-12-08 10:58:57 +0530

    portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -14,12 +14,30 @@ with c3:
 c1,c2=st.columns([1,10])
 with c2:
     st.markdown('I am Aravindh from Erode, Currently pursuing B.Tech Artificial Intelligence& Data Science 1st year in K.S.Rangasamy College of Technology.')
-
-
     st.markdown('I am always interest in developing my skills')
 st.header('Skills')
+c1,c2=st.columns([1,10])
+with c2:
+    st.subheader('Languages')
+c1,c2,c3=st.columns([1,1,1])
+with c2:
+    st.header('C')
+    progress_value = 35
+    st.markdown(f'{progress_value}%')
+    st.progress(progress_value)
+with c3:
+    st.header('Python')
+    progress_value = 35
+    st.markdown(f'{progress_value}%')
+    st.progress(progress_value)
+c1,c2=st.columns([1,10])
+with c2:
+    st.subheader('Frame work')
+c1,c2=st.columns([1,1])
+with c2:
+    st.header('Streamlit-Python')
 st.header('Projects')
-col1,col2=st.columns([1,10])
+col1,col2=st.columns([3,10])
 with col1:
     pass
 with col2:
